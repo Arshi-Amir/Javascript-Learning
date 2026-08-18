0b9baf2c1f966c333694753ef509b8db06479d92
Arshi - SDET <80699749+Arshi-Amir@users.noreply.github.com>
2026-08-18 15:14:44 +0000

    comparisons

---
+++ Basics/comparison.js
@@ -0,0 +1,12 @@
+// > , < , == , === , != , !== , >= , <=
+
+
+// 1. == (double equal) is used to compare values only 
+console.log ( "Arshi" == 'arshi');
+console.log("1" == 1);// it will return true because it will convert string to number and then compare
+
+
+// 2. === (triple equal) is used to compare values and data types
+console.log ( 13 === 13);
+
+
