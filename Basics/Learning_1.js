df123959859c657806de4a00ebbcc0adaa9b6035
Arshi - SDET <80699749+Arshi-Amir@users.noreply.github.com>
2026-08-18 15:43:57 +0000

    added theory

--- Basics/Learning_1.js
+++ Basics/Learning_1.js
@@ -1,1 +1,3 @@
 console.log("Pushing code from Codespace");
+// JS is dynamically typed language, which means you don't have to declare the data type of a variable when you create it.
+//  The data type is determined automatically based on the value assigned to the variable. 
--- Basics/dataTypes.js
+++ Basics/dataTypes.js
@@ -23,3 +23,14 @@ console.table([name, age, isLoggedIn, address, phoneNumber]);
 console.log(typeof name);
 console.log(typeof null);
 console.log(typeof undefined);
+
+
+
+// On the basis of how they are stored in memory and how they are accessed datatypes are divided into 2 categories:
+
+// 1. Primitive data types : string (values are changed in its copy not the original) , number , boolean , null , undefined , symbol , bigint
+
+// 2. Non-primitive/Reference data types : Array , Object , Function 
+// (values are changed in the original copy not the copy)
+// typeof of non primitive data types will return object for all of them except function which will return function.
+
